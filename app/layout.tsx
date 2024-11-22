982d6be5937cecb305d390a2b737a84abda97344
Jose M. Gomez <gomezjosem@live.com>
2024-11-22 01:07:55 +0000

    update imports for deno

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import "./global.css";
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
-import { Navbar } from "./components/nav";
+import { Navbar } from "./components/nav.tsx";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Footer from "./components/footer";
-import { ThemeProvider } from "./components/theme-switch";
-import { metaData } from "./config";
+import Footer from "./components/footer.tsx";
+import { ThemeProvider } from "./components/theme-switch.tsx";
+import { metaData } from "./config.ts";
+import React from "react";
 
 export const metadata: Metadata = {
   metadataBase: new URL(metaData.baseUrl),
@@ -45,7 +46,7 @@ export const metadata: Metadata = {
   },
 };
 
-const cx = (...classes) => classes.filter(Boolean).join(" ");
+const cx = (...classes: unknown[]) => classes.filter(Boolean).join(" ");
 
 export default function RootLayout({
   children,
