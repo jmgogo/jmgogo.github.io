3d4528510e13b978c0a403e0f817fc446cb784ba
Jose M. Gomez <gomezjosem@live.com>
2025-01-25 23:04:24 +0000

    hero update for name and description pending...

--- mysite/components/hero.tsx
+++ mysite/components/hero.tsx
@@ -19,14 +19,13 @@ export default function Hero() {
           <div className="pt-10 mx-auto max-w-7xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-20 lg:px-8 xl:pt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Welcome to my</span>{" "}
-                <span className="block text-indigo-600 xl:inline">
-                  Portfolio
+                <span className="block xl:inline ">hi im</span>{" "}
+                <span className="block text-8xl text-indigo-600 xl:inline">
+                  jose
                 </span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Showcase your projects, skills, and achievements in this
-                beautifully designed portfolio website.
+                I make websites.
               </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
