75904f2e978ffd2768da53eebaf5c446e7e33171
Jose M. Gomez <gomezjosem@live.com>
2025-02-28 02:29:38 +0000

    trying out the grid

---
+++ mysite/app/(sandbox)/grid/page.tsx
@@ -0,0 +1,9 @@
+import GridPaper from "@/components/grid-paper";
+
+export default function Page() {
+  return (
+    <div className="h-screen w-screen">
+      <GridPaper children={undefined} />
+    </div>
+  );
+}
