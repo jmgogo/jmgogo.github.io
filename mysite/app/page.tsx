28ff0054f2bdd1678750a01a3eb516aff1001aa0
Jose M. Gomez <gomezjosem@live.com>
2025-05-14 01:47:39 +0000

    reinit project

--- mysite/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-100">
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        Hello World
-      </main>
-    </div>
-  );
-}
