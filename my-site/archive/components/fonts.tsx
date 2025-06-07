ab21efe7c329937ceaa0541e336f98c9802a80f3
Jose M. Gomez <gomezjosem@live.com>
2025-06-07 18:16:27 +0000

    move archive into my site

---
+++ my-site/archive/components/fonts.tsx
@@ -0,0 +1,14 @@
+export function Fonts() {
+  return (
+    <div className="p-8 space-y-6">
+      <h1 className="text-4xl font-bold">Font Styles</h1>
+      <div className="text-lg space-y-4">
+        <p className=" font-light italic">This is Compagnon Light Italic.</p>
+        <p className=" font-light">This is Compagnon Light.</p>
+        <p className=" font-normal">This is Compagnon Roman.</p>
+        <p className=" font-bold italic">This is Compagnon Bold.</p>
+        <p className=" font-bold">This is Compagnon Medium.</p>
+      </div>
+    </div>
+  );
+}
